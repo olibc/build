b5e96911cacf6a845692ecc3ac8ce86890553919
Kito Cheng <kito@0xlab.org>
2013-03-23 23:07:11 +0800

    Unify indent and add comment for olibc.mk

--- core/olibc.mk
+++ core/olibc.mk
@@ -1,36 +1,48 @@
+# -----------------------------------------------------------------
 # Transform CPU Variant to TARGET_CPU_VARIANT
+#
 
 ifeq ($(strip $(TARGET_ARM_GENERIC)),true)
-TARGET_CPU_VARIANT := generic
+  TARGET_CPU_VARIANT := generic
 endif
 
 ifeq ($(strip $(TARGET_ARM_CORTEX_A9)),true)
-TARGET_CPU_VARIANT := cortex-a9
+  TARGET_CPU_VARIANT := cortex-a9
 endif
 
 ifeq ($(strip $(TARGET_ARM_CORTEX_A15)),true)
-TARGET_CPU_VARIANT := cortex-a15
+  TARGET_CPU_VARIANT := cortex-a15
 endif
 
 ifeq ($(strip $(TARGET_ARM_KRAIT)),true)
-TARGET_CPU_VARIANT := krait
-TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
+  TARGET_CPU_VARIANT := krait
+  TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
 endif
 
 ifeq ($(strip $(TARGET_ARM_SCORPION)),true)
-TARGET_CPU_VARIANT := scorpion
-TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
+  TARGET_CPU_VARIANT := scorpion
+  TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
 endif
 
 ifeq ($(strip $(TARGET_ARM_SPARROW)),true)
-TARGET_CPU_VARIANT := sparrow
-TARGET_USE_SPARROW_BIONIC_OPTIMIZATION := true
+  TARGET_CPU_VARIANT := sparrow
+  TARGET_USE_SPARROW_BIONIC_OPTIMIZATION := true
 endif
+
+# -----------------------------------------------------------------
 
 ifeq ($(ASYNC_UNWIND_TABLE)),true)
   OLIBC_CFLAGS += -fasynchronous-unwind-tables
 endif
 
+# -----------------------------------------------------------------
+#
+# This hack is for prevent Makefile dependency broken when OUT_DIR is set
+#
+# - kconf will generate string along with quote but it might break
+#   Makefile dependency due to the fact that its dependency is generated
+#   by the compiler which is not specified with quote.
+#
 ifneq (,$(strip $(OUT_DIR)))
   OUT_DIR := $(subst ",,$(OUT_DIR))
 endif
