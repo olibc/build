dfef807675d74938a566aa84fba2a19447f3dfb5
Kito Cheng <kito@0xlab.org>
2013-04-09 23:02:08 +0800

    BUILD: Fix build for cortex-a15
     - Work aroud for Crotex-A15 since -mcpu=cortex-a15 is conflict
       with -march=armv7-a,armv7...

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -9,10 +9,18 @@ ARCH_ARM_HAVE_NEON              := true
 # Note: Hard coding the 'tune' value here is probably not ideal,
 # and a better solution should be found in the future.
 #
+ifeq ($(strip $(TARGET_ARM_CORTEX_A15)),true)
+# Wrok around for Crotex-A15 since -mcpu=cortex-a15 is conflict
+# with -march=armv7-a,armv7
+arch_variant_cflags := \
+    -mfloat-abi=softfp \
+    -mfpu=neon
+else
 arch_variant_cflags := \
     -march=armv7-a \
     -mfloat-abi=softfp \
     -mfpu=neon
+endif
 
 arch_variant_ldflags := \
 	-Wl,--fix-cortex-a8
