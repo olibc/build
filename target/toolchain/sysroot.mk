79d57e6a03b83172b538336c67ed3616e7234816
Kito Cheng <kito@0xlab.org>
2013-05-25 00:37:47 +0800

    Move the sysroot to $(PRODUCT_OUT)/toolchain/

--- target/toolchain/sysroot.mk
+++ target/toolchain/sysroot.mk
@@ -1,3 +1,3 @@
-TARGET_SYSROOT := $(PRODUCT_OUT)/sysroot
+TARGET_SYSROOT := $(PRODUCT_OUT)/toolchain/sysroot
 include build/target/toolchain/sysroot-headers.mk
 include build/target/toolchain/sysroot-libs.mk
